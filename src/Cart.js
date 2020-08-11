50e3ba4b5a6febc1466e381f6015bc3459184ca5
Prachi <prachih3004@gmail.com>
2020-08-11 18:50:02 -0500

    added styling to the cart contents

--- src/Cart.js
+++ src/Cart.js
@@ -1,5 +1,6 @@
 import React, {useState} from 'react'
 import CartItem from './CartItem'
+import './Cart.css'
 
 function Cart({initialItems})
 {
@@ -19,13 +20,13 @@ function Cart({initialItems})
 	const grandTotal = items.reduce((total,item) => (total + item.price*item.qty),0).toFixed(2);
 	return(
 		<div className='Cart'>
-		<h1> Shopping Cart</h1>
+		<h1 className= 'Cart-Title'> Shopping Cart</h1>
 		<div className="Cart-items">
 		{items.map(item => (
 			<CartItem key={item.id} updateQty={updateQty} {...item} />
 			)) }
 		</div>
-		<h2>Grand Total ${grandTotal}</h2>
+		<h2 className= 'Cart-Total'>Grand Total ${grandTotal}</h2>
 		</div>
 		)
 }
